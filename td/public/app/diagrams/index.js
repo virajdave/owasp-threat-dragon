8bd95475fece2102694bf4d733f7ec9b01924e33
mike-goodwin <mike.goodwin@owaps.org>
2017-03-02 19:30:56 +0000

    updated diagrams index file

--- td/public/app/diagrams/index.js
+++ td/public/app/diagrams/index.js
@@ -3,4 +3,3 @@ var elementPropertyDirectives = require('./elementpropertydirectives');
 app.directive('tmtModalClose', [elementPropertyDirectives.modalClose]);
 app.directive('tmtElementProperties', [elementPropertyDirectives.elementProperties]);
 app.directive('tmtElementThreats', ['$routeParams', '$location', 'common', 'dialogs', elementPropertyDirectives.elementThreats]);
-app.controller('diagram', ['$scope', '$location', '$routeParams', '$timeout', 'dialogs', 'common', 'datacontext', 'threatengine', 'diagramming', require('./diagram')]);
